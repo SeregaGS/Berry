fbc32aa698d0326941aad2d52787c153954ef6f7
SeregaGS <serega.gaskarov@rambler.ru>
2025-12-31 00:43:42 +0500

    небольшая реализация навигации для мобильных устройств

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,12 @@
 /* в этот файл добавляет скрипты*/
+const burgerButton = document.querySelector('.burger-button');
+const burgerText = burgerButton.querySelector('.visually-hidden');
+const navigation = document.querySelector('.navigation__container');
+
+burgerButton.addEventListener('click', () => {
+  const isOpen = burgerButton.classList.contains('burger-button--open');
+  burgerText.textContent = isOpen ? 'Открыть меню' : 'Закрыть меню';
+  burgerButton.classList.toggle('burger-button--open');
+  navigation.classList.toggle('navigation__container--open');
+});
+
