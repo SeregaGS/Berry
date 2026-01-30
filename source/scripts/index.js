38da7a8118b06ed6863b5aa8c3aa4cf61872f291
SeregaGS <serega.gaskarov@rambler.ru>
2026-01-30 19:03:42 +0500

    fix + import => index.js

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,12 +1,5 @@
-/* в этот файл добавляет скрипты*/
-const burgerButton = document.querySelector('.burger-button');
-const burgerText = burgerButton.querySelector('.visually-hidden');
-const navigation = document.querySelector('.navigation__container');
+import { burger } from './burger.js';
+import { mapCreate } from './map.js';
 
-burgerButton.addEventListener('click', () => {
-  const isOpen = burgerButton.classList.contains('burger-button--open');
-  burgerText.textContent = isOpen ? 'Открыть меню' : 'Закрыть меню';
-  burgerButton.classList.toggle('burger-button--open');
-  navigation.classList.toggle('navigation__container--open');
-});
-
+burger();
+mapCreate();
