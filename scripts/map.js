ddaeb301156b2a4165d6e37b02944e4c146b0d94
SeregaGS <serega.gaskarov@rambler.ru>
2026-02-03 19:59:10 +0500

    Build update: 02/03/2026 19:59:10

---
+++ scripts/map.js
@@ -0,0 +1,5 @@
+var c="https://api.maptiler.com/maps/dataviz-v4/{z}/{x}/{y}.png?key=my5KOkIt5CksOOCacAKQ";var a={lat:59.91352,lng:30.3321,view:13.9},r={lat:59.9138,lng:30.33352},i="\u041D\u0430\u0431\u0435\u0440\u0435\u0436\u043D\u0430\u044F \u041E\u0431\u0432\u043E\u0434\u043D\u043E\u0433\u043E \u043A\u0430\u043D\u0430\u043B\u0430, 101.",m=L.divIcon({className:"customMarkerIcon",iconAnchor:[32,32],popupAnchor:[-15,-24],html:`<div style="
+    mask-image: url('./icons/stack.svg#leaf-icon');
+    width: 32px;
+    height: 32px;
+    background-color: #6A107A;"></div>`}),p=(o,t,n,e)=>{let{lat:s,lng:l}=e;return L.marker({lat:s,lng:l},{icon:t}).addTo(o).bindPopup(n)},u=()=>{let o=document.querySelector(".map");if(o.classList.contains("map-no-js")&&o.classList.remove("map-no-js"),!o)return null;let t=L.map(o,{zoomControl:!1,attributionControl:!1,dragging:!1,scrollWheelZoom:!1,doubleClickZoom:!1,touchZoom:!1}).setView({lat:a.lat,lng:a.lng},a.view);return L.tileLayer(c,{attribution:""}).addTo(t),p(t,m,i,r),t};export{u as mapCreate};
